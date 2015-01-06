dd76c67c207a418dd34a64c32baeafc607cf4b0a
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-01-06 10:43:15 +0100

    Init pi

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import random
 import numpy as np
 #import scipy as sp
 
-# Symbols for switch positions and signals
+### Symbols for switch positions and signals
 s0 = 0
 sL = 1
 sR = 2
@@ -14,29 +14,36 @@ s0R = 1
 sL0 = 2
 sR0 = 3
 
-# HMM parameters
-N  = 4                                   # Number of states (positions)
+### HMM parameters
+N  = 12                                  # Number of states (positions, (v, e) => v x 3)
 M  = 4                                   # Number of possible observations
 T  = 8                                   # Number of observations
 A  = np.matrix(np.ones(shape = (N, N)))  # Transition matrix (positions, always 1 due to fixed switches)
 B  = np.matrix(np.zeros(shape = (N, M))) # Emission matrix
-pi = np.matrix(np.zeros(shape = (0, N))) # Initial state probability distribution
+pi = np.matrix(np.zeros(shape = (1, N))) # Initial state probability distribution
 O  = [0, 1, 3, 2, 1, 0, 2, 3]            # Observation sequence (signals)
 
-# Model parameters
-G = np.matrix([[sX, s0, sL, sR],  # Graph of track and switches
+### Model parameters
+# Graph over switches, switch x to y may be different from y to x
+G = np.matrix([[sX, s0, sL, sR],
                [sR, sX, sL, s0],
                [s0, sR, sX, sL],
                [s0, sR, sL, sX]])
-p = 0.05                          # Probability of faulty signal
+p = 0.05 # Probability of faulty signal
 
 def init_hmm():
     # Init matrix of size N x T and fill first column
     c = np.zeros(shape = (N, T))
-    c[0].fill(1 / N)
+    c[0].fill(1 / N) # Uniform prior
     c = np.matrix(c)
     c = c.getT()
     print(c)
+
+    # Init initial prob dist matrix of size 0 x N
+    pi = np.zeros(shape = (1, N))
+    pi[0].fill(1 / N) # Uniform prior
+    pi = np.matrix(pi)
+    print(pi)
 
 if __name__ == '__main__':
     random.seed()
