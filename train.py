ddc60d8939b05d99ce5f6505c7accc32b97cdbd2
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-01-06 09:21:43 +0100

    Add graph/switch structure

--- train.py
+++ train.py
@@ -4,16 +4,30 @@ import random
 import numpy as np
 import scipy as sp
 
+# Symbols for switch positions
+s0 = 0
+sL = 1
+sR = 2
+sX = np.nan # No switch/edge
+
 # HMM parameters
-N  = 10 # Number of states (positions)
-M  = 10 # Number of possible observations
-T  = 10 # Number of observations
-A  = np.matrix(np.ones(shape = (N, N))) # Transition matrix, always 1 since switches are fixed
+N  = 4                                   # Number of states (positions)
+M  = 3                                   # Number of possible observations
+T  = 5                                   # Number of observations
+A  = np.matrix(np.ones(shape = (N, N)))  # Transition matrix (positions, always 1 due to fixed switches)
 B  = np.matrix(np.zeros(shape = (N, M))) # Emission matrix
 pi = np.matrix(np.zeros(shape = (0, N))) # Initial state probability distribution
+O  = [0, 1, 2, 2, 0]                     # Observation sequence (signals)
 
-def hmm():
-    # Init matrix of size S x T and fill first column
+# Model parameters
+G = np.matrix([[sX, s0, sL, sR],  # Graph of track and switches
+               [sR, sX, sL, s0],
+               [s0, sR, sX, sL],
+               [s0, sR, sL, sX]])
+p = 0.05                          # Probability of faulty signal
+
+def init_hmm():
+    # Init matrix of size N x T and fill first column
     c = np.zeros(shape = (N, T))
     c[0].fill(1 / N)
     c = np.matrix(c)
@@ -22,4 +36,4 @@ def hmm():
 
 if __name__ == '__main__':
     random.seed()
-    hmm()
+    init_hmm()
