aad703f0765b6ac7df6fdaaa4e3ae5966cdd465a
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-01-07 10:58:22 +0100

    Add inits for all matrices

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ N  = 12                                  # Number of states (positions, (v, e) =
 M  = 4                                   # Number of possible observations
 T  = 8                                   # Number of observations
 A  = np.matrix(np.ones(shape = (N, N)))  # Transition matrix (positions, always 1 due to fixed switches)
-B  = np.matrix(np.zeros(shape = (N, M))) # Emission matrix
+B  = np.matrix(np.zeros(shape = (N, M))) # Observation matrix
 pi = np.matrix(np.zeros(shape = (1, N))) # Initial state probability distribution
 O  = [0, 1, 3, 2, 1, 0, 2, 3]            # Observation sequence (signals)
 
@@ -36,18 +36,31 @@ Initialise the HMM.
 """
 def init_hmm():
     # Init matrix of size N x T and fill first column
-    c = np.zeros(shape = (N, T))
-    c[0].fill(1 / N) # Uniform prior
-    c = np.matrix(c)
-    c = c.getT()
+    # c = np.zeros(shape = (N, T))
+    # c[0].fill(1 / N) # Uniform prior
+    # c = np.matrix(c)
+    # c = c.getT()
     # print(c)
+
+    # Init transition matrix of size N x N
+    # TODO Not row stochastic
+    global A
+    A = np.matrix(np.ones(shape = (N, N))) # Fixes switches
+    print(A)
+
+    # Init observation matrix of size N x M
+    # TODO Not row stochastic
+    global B
+    B = np.matrix(np.zeros(shape = (N, M)))
+    B.fill(1 / 2) # Prior for all switches
+    print(B)
 
     # Init initial prob dist matrix of size 0 x N
     global pi
     pi = np.zeros(shape = (1, N))
-    pi[0].fill(1 / N) # Uniform prior
+    pi.fill(1 / N) # Uniform prior
     pi = np.matrix(pi)
-    # print(pi)
+    print(pi)
 
 """
 Recursively calculates the c value.
